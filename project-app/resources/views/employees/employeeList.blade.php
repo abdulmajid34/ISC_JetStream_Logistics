8e81d156a67b04e649204631503215f15445e188
abdulmajid <abdulmajid08180881@gmail.com>
2024-08-20 19:26:09 +0700

    added filter search employees page

--- project-app/resources/views/employees/employeeList.blade.php
+++ project-app/resources/views/employees/employeeList.blade.php
@@ -11,49 +11,56 @@
         @endif
         <h1>Employee List</h1>
         <div class="row" style="margin-top: 3rem">
-            <div class="col-9">
+            <form action="{{ route('employees') }}">
                 <div class="row">
-                    <div class="col">
-                        <div>
-                            <label for="filter_name" class="form-label">Employee Name</label>
-                            <input type="text" class="form-control" id="filter_name" name="name" placeholder="name">
+
+                    <div class="col-9">
+                        <div class="row">
+                            <div class="col">
+                                <div>
+                                    <label for="filter_name" class="form-label">Employee Name</label>
+                                    <input type="text" class="form-control" id="filter_name" name="name"
+                                        placeholder="name" value="{{ request('name') }}">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div>
+                                    <label for="filter_phone" class="form-label">Phone</label>
+                                    <input type="text" class="form-control" id="filter_phone" name="phone"
+                                        placeholder="phone" value="{{ request('phone') }}">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div>
+                                    <label for="filter_position" class="form-label">Position</label>
+                                    <input type="text" class="form-control" id="filter_position" name="position"
+                                        placeholder="position" value="{{ request('position') }}">
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div>
+                                    <label for="filter_hiredate" class="form-label">Hire Date</label>
+                                    <input type="date" class="form-control" id="filter_hiredate" name="hire_date"
+                                        placeholder="*month" value="{{ request('hire_date') }}">
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div>
-                            <label for="filter_phone" class="form-label">Phone</label>
-                            <input type="text" class="form-control" id="filter_phone" name="phone" placeholder="phone">
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div>
-                            <label for="filter_position" class="form-label">Position</label>
-                            <input type="text" class="form-control" id="filter_position" name="position"
-                                placeholder="position">
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div>
-                            <label for="filter_hiredate" class="form-label">Hire Date</label>
-                            <input type="date" class="form-control" id="filter_hiredate" name="hire_date"
-                                placeholder="*month">
-                        </div>
+                    <div class="col-3 mt-4">
+                        <button class="btn btn-primary" type="submit">Search</button>
+                        <button class="btn btn-warning">Export</button>
+                        @can('role', ['supervisor'])
+                            @if (URL::current() == route('employees'))
+                                <button class="btn btn-success">
+                                    <a href="{{ route('createForm') }}" style="color: white; text-decoration: none">
+                                        Create
+                                    </a>
+                                </button>
+                            @endif
+                        @endcan
                     </div>
                 </div>
-            </div>
-            <div class="col-3 mt-4">
-                <button class="btn btn-primary">Search</button>
-                <button class="btn btn-warning">Export</button>
-                @can('role', ['supervisor'])
-                    @if (URL::current() == route('employees'))
-                        <button class="btn btn-success">
-                            <a href="{{ route('createForm') }}" style="color: white; text-decoration: none">
-                                Create
-                            </a>
-                        </button>
-                    @endif
-                @endcan
-            </div>
+            </form>
 
 
 
@@ -75,7 +82,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($employeeList as $list)
+                            @forelse ($employees as $list)
                                 <tr>
                                     <td class="text-start">{{ $list->name }}</td>
                                     <td class="text-start">{{ $list->email }}</td>
@@ -101,7 +108,11 @@
                                         @endif
                                     @endcan
                                 </tr>
-                            @endforeach
+                            @empty
+                                <tr>
+                                    <td colspan="6" class="text-center">No Employees Found</td>
+                                </tr>
+                            @endforelse
                         </tbody>
                     </table>
 
